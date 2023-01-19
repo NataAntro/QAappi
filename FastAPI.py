356562bd7db66cd324c9c7dd236ac6b25e7a7131
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-19 07:32:49 +0300

    Update FastAPI.py

--- FastAPI.py
+++ FastAPI.py
@@ -1,19 +1,31 @@
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
+model_name = "AndrewChar/model-QA-5-epoch-RU"
+
+# Получение предсказаний
+# pipeline вопрос-ответ
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name) # nlp - переменная для хранения созданного конвейера
+
+# Загрузка модели и токенайзера
+model = AutoModelForQuestionAnswering.from_pretrained(model_name, from_tf=True)
+
 from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import JSONResponse
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import uvicorn
 
 app = FastAPI()
 
+
 @app.post("/qa")
 async def question_answering(request: Request):
-    
     data = await request.json()
-    
+
     question = data["question"]
     context = data["context"]
-   
-    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
     res = nlp({'question': question, 'context': context})
-    
+
     return JSONResponse(content={"answer": res.get("answer")}, status_code=status.HTTP_200_OK)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
