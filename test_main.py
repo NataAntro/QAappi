16a28c1c761a290b7044263a04e21d51ef1c8685
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-24 18:15:18 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,6 +6,7 @@ client = TestClient(app)
 
 def test_question_answering():
     response = client.get("/predict/")
+    assert response.status_code == 200
     response_json = response.json()
     assert "question" in response_json
     assert response_json["question"] == "Что такое API?"
@@ -21,8 +22,9 @@ def test_example():
     'question': 'Что такое API?',
     'context': 'API — описание способов взаимодействия одной компьютерной программы с другими.'
 }
-    res = nlp(QA_input)
-    response = client.post("/predict/", json={"answer": res.get('answer')})
-    assert response.json() == {"answer": 'описание способов взаимодействия одной компьютерной программы'}
-   
-    
+res = nlp(QA_input)
+response = client.post("/predict/", json={"answer": res.get('answer')})
+assert response.status_code == 200
+response_json = response.json()
+assert "answer" in response_json
+assert response_json["answer"] == 'описание способов взаимодействия одной компьютерной программы'
