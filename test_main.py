d0e49e571f1e6e9133bae45937044362fa7f5abe
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-24 19:26:38 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,15 +1,15 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/predict/")
+def test_qa():
+    response = client.get("/predict/",
     QA_input = {
         'question': 'Что такое API?',
-        'context': 'API — описание способов взаимодействия одной компьютерной программы с другими.'
-    }
+        'context': 'API — описание способов взаимодействия одной компьютерной программы с другими.'}
+    )
     json_data = response.json() 
                           
     assert response.status_code == 200
