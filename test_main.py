62a1480258d2375f500c07a35aec9697b5cbf119
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-24 17:51:56 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,24 +5,20 @@ from main import app
 client = TestClient(app)
 
 def test_question_answering():
-    # Инициализация конвейера вопрос-ответ
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"question": "Что такое API?"}
+    
+def test_example():
+    model_name = "AndrewChar/model-QA-5-epoch-RU"
     nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-    # Задание входных данных для конвейера
-    QA_input = {'question': question, 'context': context}
-    # Получение ответа от конвейера
+    QA_input = {
+    'question': 'Что такое API?',
+    'context': 'API — описание способов взаимодействия одной компьютерной программы с другими.'
+}
     res = nlp(QA_input)
-    # Загрузка модели и токенайзера
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name, from_tf=True)
-    # Задание входных данных для модели
-    question_set = {'question': question, 'context': context}
-    # Получение ответа от модели
-    res = nlp(question_set)
-    # Возврат ответа
-    return res.get('answer')
-
-# Пример использования
-model_name = "AndrewChar/model-QA-5-epoch-RU"
-question = "Что такое API?"
-context = "API — описание способов взаимодействия одной компьютерной программы с другими."
-answer = test_question_answering(model_name, question, context)
-print(answer)
+    response = client.post("/predict/", json={"answer": res.get('answer')})
+    assert response.status_code == 200
+    assert response.json() == {"answer": res.get('answer')}
+   
+    
