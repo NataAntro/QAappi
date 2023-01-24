646f3e5b0b3b5014feb278fa65f7ba51a83567b6
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-24 18:26:05 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,9 +5,8 @@ from main import app
 client = TestClient(app)
 
 def test_question_answering():
-    response = client.get("/predict/")
+    response = client.post("/predict/",
     json={"context": "API — описание способов взаимодействия одной компьютерной программы с другими."}
-    )
     json={"question": "Что такое API?"}
     )
     json_data = response.json() 
