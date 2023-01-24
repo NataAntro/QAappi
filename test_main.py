0f6e975fde502e5ecc484185e05a2126024f4cc1
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-01-24 18:23:59 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,12 +6,12 @@ client = TestClient(app)
 
 def test_question_answering():
     response = client.get("/predict/")
+    json={"context": "API — описание способов взаимодействия одной компьютерной программы с другими."}
+    )
+    json={"question": "Что такое API?"}
+    )
+    json_data = response.json() 
+
     assert response.status_code == 200
-    response_json = response.json()
-    assert "question" in response_json
-    assert response_json["question"] == "Что такое API?"
-    assert "context" in response_json
-    assert response_json["context"] == "API — описание способов взаимодействия одной компьютерной программы с другими."
-    assert "answer" in response_json
     assert response_json['answer'] == 'описание способов взаимодействия одной компьютерной программы'
     
