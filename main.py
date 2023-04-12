0736a07ea2a73405fb4a5388b40293f97fc5a4f0
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-04-12 18:45:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 app = FastAPI()
 model_name = "AndrewChar/model-QA-5-epoch-RU"
 
+
 @app.get("/predict")
 def predict():
-    """Анализ текста по контексту в формате вопрос-ответ"""
+    """Анализ текста по контексту в формате вопрос-ответ."""
     nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-    QA_input = {
+    qa_input = {
         'question': 'Что такое API?',
         'context': 'API — описание способов взаимодействия одной компьютерной программы с другими.'
     }
-    res = nlp(QA_input)
-    return {"answer": res.get('answer')}
+    result = nlp(qa_input)
+    return {"answer": result.get('answer')}
